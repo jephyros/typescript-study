cfb4eb160ba964ac7d49c203679f457e1a4d6cca
cis-WIn <kr.chis@gmail.com>
2020-01-03 17:37:33 +0900

    #10 ing

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,53 @@
-interface Human  {
-    name:string,
-    age:number,
-    gender:string
+import * as CryptoJS from "crypto-js";
 
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => {
+        return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    }
+
+    constructor(index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const person ={
-    name :"ChoiInSeok",
-    age:45,
-    gender :"Male"
+const genesisBlock: Block = new Block(0, "2020202020202020", "", "Hello", 123465);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string):Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimeStamp: number = getNewTimeStamp();
+    const newHash : string = Block.calculateBlockHash(newIndex,previousBlock.hash,newTimeStamp,data);
+
+    const newBlock :Block = new Block(newIndex,newHash,previousBlock.hash,data,newTimeStamp);
+
     
+    return newBlock;
 
 }
-const sayHi = (persion:Human):string => {
-    //console.log(`Hello ${name} , you are ${age}, your are a ${gender}`);
-    return `Hello ${person.name} , you are ${person.age}, your are a ${person.gender}`;
 
-}    
-console.log(`debug : `,sayHi(person));
+console.log(createNewBlock("firstBlock"),"\n---------------",createNewBlock("secondBlock"))
 
-export{}    
+//console.log(blockchain[0]);
+
+export { }    
